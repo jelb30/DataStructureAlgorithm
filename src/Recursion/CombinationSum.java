e81e8de4d1d6e6e69e5fc9a7ea75bafbb26326fe
Jill Patel <pateljill2288@gmail.com>
2025-04-01 17:38:08 -0700

    Cleaning Main method

--- src/Recursion/CombinationSum.java
+++ src/Recursion/CombinationSum.java
@@ -98,4 +98,20 @@ public class CombinationSum {
         }
     }
 
+    public static class Factorial {
+
+        public void Implementation(){
+            int n = 19;
+            int factorialVal = factorialByRecursion(n);
+            System.out.println(factorialVal);
+        }
+
+        public int factorialByRecursion(int n){
+            if(n ==0){
+                return 1;
+            }
+            return n*factorialByRecursion(n-1);
+        }
+
+    }
 }
